6e3834b60b3e35ec957b9db36532773aeb241453
Gabriela Midori Restani <gabriela.restani@redealumni.com>
2023-08-19 16:02:55 -0300

    project initial setup

---
+++ components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Hello World!
+  </div>
+  <q-circular-progress
+      :value="value"
+      size="90px"
+      :thickness="0.2"
+      color="orange"
+      center-color="grey-8"
+      track-color="transparent"
+      class="q-ma-md"
+    />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref(50);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
