0042254f1d6a10277c695ad49f6cbab2fe9a0df8
Gabriela Midori Restani <gabriela.restani@redealumni.com>
2023-08-20 03:32:08 -0300

    feat: add on boarding component into home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <Home />
+  <OnBoarding />
 </template>
 <script setup>
 import Home from '~/components/home';
+import OnBoarding from '~/components/on-boarding/on-boarding';
+import { onMounted } from 'vue';
+import { StorageSerializers, useStorage } from '@vueuse/core';
+
+onMounted(() => {
+  const storage = useStorage('eita-frontend', null, undefined, { serializer: StorageSerializers.object }); 
+  storage.value.interests = ['Vue', 'Nuxt', 'Quasar'];
+  storage.value.user = { name: 'John Doe', age: 30 };
+});
 </script>
