ce0edaa55ac23ae7dcda16272b69277bd178c915
vitebo <andre.vitebo@quero.com>
2023-08-20 10:23:27 -0300

    change scroll color button

--- components/layout/content.vue
+++ components/layout/content.vue
@@ -3,8 +3,15 @@
     <q-page>
       <slot />
       <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-        <q-btn fab icon="keyboard_arrow_up" color="accent" />
+        <q-btn fab icon="keyboard_arrow_up" class="content__button" />
       </q-page-scroller>
     </q-page>
   </q-page-container>
 </template>
+
+<style scoped>
+.content__button {
+  background-color: #304FFE;
+  color: white;
+}
+</style>
