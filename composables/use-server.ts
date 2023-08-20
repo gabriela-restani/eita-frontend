062ac1fb0984ae231255855300a34a46b3b49edb
vitebo <andre.vitebo@quero.com>
2023-08-20 06:08:13 -0300

    integrate feed

--- composables/use-server.ts
+++ composables/use-server.ts
@@ -7,6 +7,35 @@ export default function useServer() {
         method: 'POST',
         body: JSON.stringify(body),
       });
+    },
+
+    getFeed(params: { userId: string }) {
+      return useFetch(`${host}/feed/index`, {
+        method: 'GET',
+        params: {
+          user_id: params.userId,
+        }
+      });
+    },
+
+    createInteraction(params: { userId: string, contentId: string, classification: 'Like' | 'Dislike' }) {
+      return useFetch(`${host}/content/classify`, {
+        method: 'POST',
+        body: JSON.stringify({
+          user_id: params.userId,
+          content_id: params.contentId,
+          classification: params.classification,
+        })
+      });
+    },
+
+    recalculateUserInterests(params: { userId: string }) {
+      return useFetch(`${host}/user/update`, {
+        method: 'POST',
+        body: JSON.stringify({
+          user_id: params.userId,
+        })
+      });
     }
   }
 }
---
+++ pages/feed.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <q-btn
+      color="purple"
+      label="Recalcular"
+      @click="recalculateUserInterests({ userId: '8' })"
+    />
+
+    <q-card
+      class="my-card"
+      v-for="item in contents"
+      :key="item.id"
+    >
+      <q-card-section>
+        {{ item.title }}
+      </q-card-section>
+
+      <q-separator dark inset />
+      
+      <q-card-section>
+        <q-btn
+          color="primary"
+          label="Like"
+          @click="createInteraction({ userId: '8', contentId: item.id, classification: 'Like' })"
+        />
+        
+        <q-btn
+          color="deep-orange"
+          label="Dislike"
+          @click="createInteraction({ userId: '8', contentId: item.id, classification: 'Dislike' })"
+        />
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { createInteraction, recalculateUserInterests, getFeed } = useServer();
+
+const { data } = await getFeed({
+  userId: '8'
+});
+
+const contents = data.value.contents;
+</script>
