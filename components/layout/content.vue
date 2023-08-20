467799a28314cd4db6f3befe75c88af6a143d1e2
Gabriela Midori Restani <gabriela.restani@redealumni.com>
2023-08-19 21:29:45 -0300

    feat: remove layout's content padding

--- components/layout/content.vue
+++ components/layout/content.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page-container>
-    <q-page padding>
+    <q-page>
       <slot />
       <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
         <q-btn fab icon="keyboard_arrow_up" color="accent" />
