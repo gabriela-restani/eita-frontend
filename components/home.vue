eb62f1f154e21aa9edeb7a6610757ee88f875564
Gabriela Midori Restani <gabriela.restani@redealumni.com>
2023-08-20 03:33:12 -0300

    quasar's elements test

--- components/home.vue
+++ components/home.vue
@@ -3,14 +3,18 @@
     Hello World!
   </div>
   <q-circular-progress
-      :value="value"
-      size="90px"
-      :thickness="0.2"
-      color="orange"
-      center-color="grey-8"
-      track-color="transparent"
-      class="q-ma-md"
-    />
+    :value="value"
+    size="90px"
+    :thickness="0.2"
+    color="orange"
+    center-color="grey-8"
+    track-color="transparent"
+    class="q-ma-md"
+  />
+
+  <p v-for="n in 100" :key="`p-${n}`">
+    Loren ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
+  </p>
 </template>
 
 <script setup>
@@ -20,5 +24,4 @@ const value = ref(50);
 </script>
 
 <style lang="scss" scoped>
-
 </style>
